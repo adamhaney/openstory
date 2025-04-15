96d7b5aab2e1efaf874e96f9b8d443f82fbfefc6
adamhaney <adam.jacob.haney@gmail.com>
2025-04-15 15:39:19 -0400

    Setup for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,12 @@ module.exports = function(eleventyConfig) {
     // Copy static files to output
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/*.png");
+    // Copy static files to output
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/*.png");
     
     return {
+        pathPrefix: "/openstory/",
         dir: {
             input: "src",
             output: "_site",
